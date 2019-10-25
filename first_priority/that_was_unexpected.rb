2f10f0e7c880b5feb58bf787b1bf343728cf282d
Michael Calkins <m.calkins576@gmail.com>
2019-10-25 17:29:39 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,13 +1,13 @@
 require 'pry'
 
 class Person
-  #attr_accessor :name, :candy_lover
-  binding.pry
+  attr_accessor :name, :candy_lover
+  
   def initialize(name, candy_lover)
     @name = name
     @candy_lover = candy_lover
   end
-
+=begin
   def tall?
     true
   end
@@ -27,5 +27,5 @@ class Person
       10.times do
         "not a ballin candy lover"
     end
-  end
+=end
 end
